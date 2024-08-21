10ee53e1b0ed84d20235894e5b77c300673973d0
Tran Van Khuyen <81480245+hoshikira18@users.noreply.github.com>
2024-08-21 13:41:44 +0700

    add: limit, offset to news api

--- medusa-config.js
+++ medusa-config.js
@@ -50,8 +50,8 @@ const modules = {
 const projectConfig = {
   jwt_secret: process.env.JWT_SECRET || "supersecret",
   cookie_secret: process.env.COOKIE_SECRET || "supersecret",
-  store_cors: process.env.STORE_CORS,
   database_url: DATABASE_URL,
+  store_cors: process.env.STORE_CORS,
   admin_cors: process.env.ADMIN_CORS,
 };
 
--- src/api/store/posts/route.ts
+++ src/api/store/posts/route.ts
@@ -5,7 +5,18 @@ export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
   const manager: EntityManager = req.scope.resolve("manager");
   const postRepo = manager.getRepository("Post");
 
+  const limit = parseInt(req.query.limit as string) || 100;
+  const offset = parseInt(req.query.offset as string) || 0;
+
+  const posts = await postRepo.find({
+    take: limit,
+    skip: offset,
+  });
+
   return res.json({
-    posts: await postRepo.find(),
+    posts: posts,
+    limit: limit,
+    offset: offset,
+    count: posts.length,
   });
 };
